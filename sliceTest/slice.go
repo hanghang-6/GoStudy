6c0b43554e2d4aa353974fc0abf2603292a732bf
hanghang-6 <ytt2361@163.com>
2024-07-21 21:06:22 +0800

    new slice from old slice

--- sliceTest/slice.go
+++ sliceTest/slice.go
@@ -6,28 +6,33 @@ import (
 	"unsafe"
 )
 
-func test(s []int) {
-	PrintSliceStructural(&s)
-}
-
 func PrintSliceStructural(s *[]int) {
 	ss := (*reflect.SliceHeader)(unsafe.Pointer(s))
 	fmt.Printf("slice struct:%+v,slice is %vn\n", ss, s)
 }
 
-func case1(c []int) {
-	c[1] = 1
-	PrintSliceStructural(&c)
+func case2(s []int) {
+	s = s[1:]
+	PrintSliceStructural(&s)
 }
-
-func case2(c []int) {
-	c = append(c, 5)
-	c[1] = 1
-	PrintSliceStructural(&c)
+func case1(s []int) {
+	s = s[1:3]
+	PrintSliceStructural(&s)
+}
+func case3(s []int) {
+	s = s[len(s)-1:]
+	PrintSliceStructural(&s)
+}
+func case4(s []int) {
+	ss := s[1:]
+	PrintSliceStructural(&ss)
 }
 func main() {
-	
-	s := make([]int, 5, 10)
+
+	s := make([]int, 9)
 	case2(s)
+	case1(s)
+	case3(s)
+	case4(s)
 	PrintSliceStructural(&s)
 }
