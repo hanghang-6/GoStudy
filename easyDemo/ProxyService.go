4b3dafb07ef75205d3bae94e8d7a966456e1b740
zzh <ytt2361@163.com>
2024-08-07 11:17:05 +0800

    proxy server
    1  认证
    2  解析包ip:port、iptype、commend等  打印

--- easyDemo/ProxyService.go
+++ easyDemo/ProxyService.go
@@ -2,24 +2,147 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net"
 )
+
+const socks5Ver = 0x05
+
+// commend = 1 表示 connection命令  即 代理服务器与目标服务建立新的TCP连接
+const cmdBind = 0x01
+const atypeIPV4 = 0x01
+const atypeHOST = 0x03
+const atypeIPV6 = 0x04
+
+func auth(reader *bufio.Reader, conn net.Conn) (err error) {
+	// +----+----------+----------+
+	// |VER | NMETHODS | METHODS  |
+	// +----+----------+----------+
+	// | 1  |    1     | 1 to 255 |
+	// +----+----------+----------+
+	// VER: 协议版本，socks5为0x05
+	// NMETHODS: 支持认证的方法数量
+	// METHODS: 对应NMETHODS，NMETHODS的值为多少，METHODS就有多少个字节。RFC预定义了一些值的含义，内容如下:
+	// X’00’ NO AUTHENTICATION REQUIRED
+	// X’02’ USERNAME/PASSWORD
+
+	// 读版本号
+	ver, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read ver failed:%w", err)
+	}
+	// 比较
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	methodSize, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read methodSize failed:%w", err)
+	}
+	method := make([]byte, methodSize)
+	//将读到的填充到创建的切片中
+	_, err = io.ReadFull(reader, method)
+	if err != nil {
+		return fmt.Errorf("read method failed:%w", err)
+	}
+	log.Println("ver", ver, "method", method)
+
+	// +----+--------+
+	// |VER | METHOD |
+	// +----+--------+
+	// | 1  |   1    |
+	// +----+--------+
+	_, err = conn.Write([]byte{socks5Ver, 0x00}) // 0x00代表不需要验证
+	if err != nil {
+		return fmt.Errorf("write failed:%w", err)
+	}
+	return nil
+}
+
+func connect(reader *bufio.Reader, conn net.Conn) (err error) {
+	// +----+-----+-------+------+----------+----------+
+	// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER 版本号，socks5的值为0x05
+	// CMD 0x01表示CONNECT请求
+	// RSV 保留字段，值为0x00
+	// ATYP 目标地址类型，DST.ADDR的数据对应这个字段的类型。
+	//   0x01表示IPv4地址，DST.ADDR为4个字节
+	//   0x03表示域名，DST.ADDR是一个可变长度的域名
+	// DST.ADDR 一个可变长度的值
+	// DST.PORT 目标端口，固定2个字节
+
+	// 大小为4byte的buffer
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return fmt.Errorf("read header failed:%w", err)
+	}
+	// 对应协议的字段 其中 RSV保留字不予理会
+	ver, cmd, atyp := buf[0], buf[1], buf[3]
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	if cmd != cmdBind {
+		return fmt.Errorf("not supported cmd:%v", cmd)
+	}
+	addr := ""
+	switch atyp {
+	case atypeIPV4:
+		// ipv4 地址长度为32bit=4byte  正好用buf接收
+		_, err = io.ReadFull(reader, buf)
+		if err != nil {
+			return fmt.Errorf("read atyp failed:%w", err)
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
+	case atypeHOST:
+		hostSize, err := reader.ReadByte()
+		if err != nil {
+			return fmt.Errorf("read hostSize failed:%w", err)
+		}
+		host := make([]byte, hostSize)
+		_, err = io.ReadFull(reader, host)
+		if err != nil {
+			return fmt.Errorf("read host failed:%w", err)
+		}
+		addr = string(host)
+	case atypeIPV6:
+		return errors.New("IPv6: no supported yet")
+	default:
+		return errors.New("invalid atyp")
+	}
+	// 读两字节  port
+	_, err = io.ReadFull(reader, buf[:2])
+	if err != nil {
+		return fmt.Errorf("read port failed:%w", err)
+	}
+	port := binary.BigEndian.Uint16(buf[:2])
+
+	log.Println("dial", addr, port)
+
+	return nil
+}
 
 func process(conn net.Conn) {
 	// 记得关闭连接
 	defer conn.Close()
 	// 带缓冲的 只读流  带缓冲的流可以减少底层系统调用的次数
 	reader := bufio.NewReader(conn)
-	for {
-		b, err := reader.ReadByte()
-		if err != nil {
-			break
-		}
-		_, err = conn.Write([]byte{b})
-		if err != nil {
-			break
-		}
+	err := auth(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed: %v", conn.RemoteAddr(), err)
+		return
+	}
+	err = connect(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed: %v", conn.RemoteAddr(), err)
+		return
 	}
 }
 func main() {
